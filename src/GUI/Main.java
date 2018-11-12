4c8f9c16df6996c0aab9079a1003ee07e55b8424
Patryk Robakowski <patryk711@outlook.com>
2018-11-12 12:17:45 +0100

    added background image

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -11,13 +11,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Candy Wars");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
