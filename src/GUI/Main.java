17d3963459ff008a7f54d43ccdc5636387490e56
Patryk Robakowski <patryk711@outlook.com>
2018-11-13 14:19:30 +0100

    main menu button functionality in progress

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,24 +1,35 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
+        Parent menu = FXMLLoader.load(getClass().getResource("main-menu-scene.fxml"));
         primaryStage.setTitle("Candy Wars");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setScene(new Scene(menu, 1280, 720));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
 
         launch(args);
     }
-}
+
+    public void renderGame(Stage gameStage) throws Exception {
+        Parent game = FXMLLoader.load(getClass().getResource("game-scene.fxml"));
+        gameStage.setTitle("Candy Wars");
+        gameStage.setScene(new Scene(game, 1280, 720));
+        gameStage.show();
+    }
+
---
+++ src/GUI/MainMenuController.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MainMenuController {
+
+
+
+    @FXML
+    void handleStartButton() throws Exception{
+        System.out.println("Button was clicked");
+
+    }
+}
