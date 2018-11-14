30fc4078db70a1a82d5d28e84d8d654ff2b7c069
Laurea <laurea.lindroos@hotmail.com>
2018-11-14 13:03:19 +0100

    Function for Quit Button

--- src/GUI/MenuController.java
+++ src/GUI/MenuController.java
@@ -1,5 +1,8 @@
 package GUI;
+import Assets.Player;
 import GUI.Main;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
@@ -7,16 +10,19 @@ public class MenuController {
 
     private Main main;
 
+    private Stage stage = new Stage();
+
     @FXML
     void handleStartButton() throws Exception {
         // Todo: See if there is a way to change scene instead of creating a new stage
        main = new Main();
-       main.renderGame(new Stage());
+       main.renderGame(stage);
        System.out.println("Button was clicked");
     }
 
     @FXML
-    void handleQuitButton() throws Exception {
+    void handleQuitButton(){
+        Platform.exit();
 
     }
 }
